9176dbb7d861b6c8e8930d13e1803bf4de7abc55
Базанов Максим Сергеевич <kirpichicus@gmail.com>
2019-04-30 03:27:56 +0300

    Добавьте файлы проекта.

---
+++ BazanovMS_Kursach_Web_Cezar.Tests/Models/DownloaderTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BazanovMS_Kursach_Web_Cezar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazanovMS_Kursach_Web_Cezar.Models.Tests
+{
+    [TestClass()]
+    public class DownloaderTests
+    {
+        [TestMethod()]
+        public void MakeDocxFileTest() //можно было бы сделать приватным и рефлексия
+        {
+            try
+            {
+                Downloader.MakeDocxFile("text\ntext", "Txt.docx");
+                Downloader.MakeDocxFile("text\ntext", "txt.txt");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod()]
+        public void MakeTxtFileTest()
+        {
+            try
+            {
+                Downloader.MakeTxtFile("text\ntext", "Txt.docx");
+                Downloader.MakeTxtFile("text\ntext", "txt.txt");
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        
+    }
+}
